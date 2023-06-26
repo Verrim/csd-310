7acac08a5eb74776d894e0d057fa41bb8afcf457
Verrim <110069390+Verrim@users.noreply.github.com>
2023-06-26 15:20:25 -0400

    Update pytech_query.py

--- module_5/pytech_query.py
+++ module_5/pytech_query.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
+url = "mongodb+srv://admin:admin@cluster0.12tpk99.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(url)
 
@@ -15,9 +15,9 @@ print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 for doc in student_list:
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
-bilbo = students.find_one({"student_id": "1008"})
+shane = students.find_one({"student_id": "1007"})
 
 print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
-print("  Student ID: " + bilbo["student_id"] + "\n  First Name: " + bilbo["first_name"] + "\n  Last Name: " + bilbo["last_name"] + "\n")
+print("  Student ID: " + shane["student_id"] + "\n  First Name: " + shane["first_name"] + "\n  Last Name: " + shane["last_name"] + "\n")
 
 input("\n\n  End of program, press any key to continue...")
