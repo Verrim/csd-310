0fa5fb8c5762055c47753ff9d7edfed04e623845
Verrim <110069390+Verrim@users.noreply.github.com>
2023-07-02 19:47:47 -0400

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,16 +1,22 @@
 #This program was written by me, however, I have coppied the format:
 
+# Import Statement
 from pymongo import MongoClient
 
+# MongoDB connection string
 url = "mongodb+srv://admin:admin@cluster0.12tpk99.mongodb.net/?retryWrites=true&w=majority"
 
+# connect MongoDB cluster
 client = MongoClient(url)
 
+# connect pytech database
 db = client.pytech
 
+# displays
 print("\n -- Pytech COllection List --")
 print(db.list_collection_names())
 
+# exit
 input("\n\n  End of program, press any key to exit... ")
 
 
